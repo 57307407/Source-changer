d644ea269e2e341945942204d8dbcda1aa1c8e9a
yufeng <321353225@qq.com>
2023-03-10 14:40:58 +0800

    修改npm工具，支持electron 换源，完善main.py注释

--- modules/npm.py
+++ modules/npm.py
@@ -13,3 +13,5 @@ sources = [
 # 镜像信息
 def use_mirror(index):
     os.system('npm config set registry={0}'.format(sources[index]))
+    print('本工具将对 electron 进行换源')
+    os.system('npm config set electron_mirror "{0}"electron/'.format(sources[index]))
